cd1609579b46c51fcc3515e877b831bca9842d3e
Karolina Guzik <kontakt.ikagi@gmail.com>
2024-03-26 21:57:09 +0100

    feat: add new projects, fix: portfolio cards styles fixes

--- components/molecules/PortfolioCard.tsx
+++ components/molecules/PortfolioCard.tsx
@@ -19,33 +19,44 @@ const PortfolioCard: FC<PortfolioCardProps> = ({ portfolio, index }) => {
         >
             <h4 className='mb-5'>{portfolio.title}</h4>
             {portfolio.description && (
-                <p className={styles.description}>{portfolio.description}</p>
+                <p
+                    className={styles.description}
+                    dangerouslySetInnerHTML={{ __html: portfolio.description }}
+                ></p>
             )}
-            <div className={styles.imageWrapper}>
-                <Image
-                    src={portfolio.image}
-                    alt={portfolio.title}
-                    fill
-                    className='object-cover'
-                />
-            </div>
-            <div className={styles.buttonWrapper}>
-                <Button text={'Live'} url={portfolio.link} target={'_blank'} />
-                {portfolio.githubLink && (
+            <div className={styles.wrapper}>
+                <div className={styles.imageWrapper}>
+                    <Image
+                        src={portfolio.image}
+                        alt={portfolio.title}
+                        fill
+                        className='object-cover'
+                    />
+                </div>
+                <div className={styles.buttonWrapper}>
                     <Button
-                        text={'GitHub'}
-                        url={portfolio.githubLink}
+                        text={'Live'}
+                        url={portfolio.link}
                         target={'_blank'}
                     />
-                )}
+                    {portfolio.githubLink && (
+                        <Button
+                            text={'GitHub'}
+                            url={portfolio.githubLink}
+                            target={'_blank'}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
 };
 
 const styles = {
-    card: 'bg-white p-6 lg:p-12 rounded-lg',
-    description: 'mb-6',
+    card: 'bg-white p-6 lg:p-12 rounded-lg flex flex-col h-full',
+    description:
+        'mb-6 [&>a]:text-violet [&>a]:transition-opacity hover:[&>a]:opacity-80',
+    wrapper: 'mt-auto',
     imageWrapper:
         'relative w-full h-[320px] rounded-lg overflow-hidden mb-4 lg:mb-6',
     buttonWrapper: 'flex flex-wrap gap-4',
